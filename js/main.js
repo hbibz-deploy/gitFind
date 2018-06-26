bbb19a207c63d1c0aa19e8270c2eb635fa81b879
Hb1bz <33839501+hbibz-deploy@users.noreply.github.com>
2018-06-26 12:28:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
+    $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up btn-primary"></span> Back to Top</div>');
     	$(window).scroll(function () {
 			if ($(this).scrollTop() !== 0) {
 				$('#toTop').fadeIn();
